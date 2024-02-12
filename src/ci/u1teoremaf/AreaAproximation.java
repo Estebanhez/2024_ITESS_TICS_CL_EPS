0a312a7760e57c729a69b3aa6a7765186df51171
EstebanPerez <perez.sanchez.esteban.1g@gmail.com>
2024-02-12 10:59:52 +0000

    Se agrego el metodo print

--- src/ci/u1teoremaf/AreaAproximation.java
+++ src/ci/u1teoremaf/AreaAproximation.java
@@ -28,9 +28,16 @@ public class AreaAproximation {
         area=0;
         for(int i=0; i<=n; i++){
             x[i]=xIni+i*dx;
-            fx[i]=x[i]*x[i];
+            //fx[i]=x[i]*x[i];
+            fx[i]=Math.sqrt(9.0-x[i]*x[i]);
             area=area+fx[i]*dx;
         }
+    }
+    public void print(){
+        for (int i = 0; i <= n; i++){
+            System.out.println(i + " "+x[i]+" "+fx[i]);
+        }
+        System.out.println("Area: "+area);
     }
 
     public double getArea() {
--- src/ci/u1teoremaf/AreaAproximationTest.java
+++ src/ci/u1teoremaf/AreaAproximationTest.java
@@ -6,8 +6,9 @@ package ci.u1teoremaf;
 
 public class AreaAproximationTest {
     public static void main(String args[]){
-        AreaAproximation ap=new AreaAproximation(0.0,4.0,5);
+        AreaAproximation ap=new AreaAproximation(-3, 3, 6);
         ap.computeArea();
+        ap.print();
         System.out.println(ap.getArea());
     }
 }
